98056279349e63a5ebbc1be4e22b97fbae8218c9
Raajitha <raajithachikka@gmail.com>
2024-10-11 22:26:42 +0530

    terraform folder

---
+++ terraform/prod/main.tf
@@ -0,0 +1,55 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_security_group" "prod_sg" {
+  name        = "prod_sg"
+  description = "Allow SSH and HTTP traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow SSH from anywhere (not recommended for production)
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow HTTP from anywhere
+  }
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"            # Allow all outbound traffic
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "prod_server" {
+  ami                    = "ami-0866a3c8686eaeeba"  # Ensure this AMI is valid for your region
+  instance_type          = "t3.medium"
+  key_name               = "raaji"                   # Replace with your key pair name
+  vpc_security_group_ids = [aws_security_group.prod_sg.id]
+
+  tags = {
+    Name = "Prod Server"
+  }
+}
+
+output "prod_server_ip" {
+  value = aws_instance.prod_server.public_ip
+}
+
+
+
+
